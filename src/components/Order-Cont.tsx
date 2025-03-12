44830e09891532c7ed23fe948a5c35486f018c3c
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-03-12 20:55:31 +0800

    .

--- src/components/Order-Cont.tsx
+++ src/components/Order-Cont.tsx
@@ -1,58 +1,114 @@
-'use client'
+"use client";
 
-import * as React from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import Paper from '@mui/material/Paper';
 
-const OrderCont = () => {
-    const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'customer', headerName: 'Customer', width: 130 },
-        { field: 'food', headerName: 'Food', width: 130 },
-        {
-          field: 'date',
-          headerName: 'date',
-          type: 'date',
-          width: 90,
-        },
-        {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-        },
-      ];
-      
-      const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-        { id: 10, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-      ];
-      
-      const paginationModel = { page: 0, pageSize: 5 };
-    return(
-    <div className='ml-[200px] px-8 w-full py-10'>
-      <DataGrid
-      className='w-full'
-        rows={rows}
-        columns={columns}
-        initialState={{ pagination: { paginationModel } }}
-        pageSizeOptions={[5, 20]}
-        checkboxSelection
-        sx={{ border: 0 }}
-      />
-    </div>
-    )
-
-    
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+type Response = {
+  _id : string
+  userData : UserData
+  orderItems : item[]
+  createdAt : Date
+  totalPrice : number,
+  status : string
 }
-export default OrderCont
+type UserData= {
+  email : string,
+  address : string
+}
+type item = {
+  food : food
+  quantity : number
+}
+type food = {
+  food_name : string
+}
+type Order = {
+  id: string;
+  index: number;
+  customer: string;
+  food: string;
+  createdAt: string;
+  totalPrice: string;
+  address: string;
+  status: string;
+};
+const OrderCont = () => {
+  const [orders, setOrders] = useState<Order[]>([])
+  const transformOrders = (data: Response[]) => {
+    return data.map((order, index) => ({
+      id : order._id, 
+      index: index + 1, 
+      customer: order.userData.email, 
+      food: order.orderItems
+        .map((item) => `${item.food.food_name} x${item.quantity}`) 
+        .join(", "), 
+      createdAt: new Date(order.createdAt).toLocaleDateString(), 
+      totalPrice: `$${order.totalPrice.toFixed(2)}`,
+      address: order.userData.address, 
+      status :order.status
+    }));
+  };
+  const getOrders = async () => {
+ 
+    try {
+      const response = await axios.get(`http://localhost:3000/foodorder/admin`);
+      console.log(response);
+      const transformedOrders = transformOrders(response.data); 
+      console.log(transformedOrders);
+      
+      setOrders(transformedOrders);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(()=>{
+    getOrders()
+  },[])
+
+
+
+
+  return (
+    <div className="ml-[200px] px-8 w-full py-10">
+      <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {orders.map((order) => (
+          <TableRow key={order.id}>
+            <TableCell className="font-medium">{order.index}</TableCell>
+            <TableCell>{order.customer}</TableCell>
+            <TableCell>{order.food}</TableCell>
+            <TableCell>{order.createdAt}</TableCell>
+            <TableCell className="text-right">{order.totalPrice}</TableCell>
+            <TableCell>{order.address} </TableCell>
+            <TableCell>{order.status}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+        </TableRow>
+      </TableFooter>
+    </Table>
+    </div>
+  );
+};
+export default OrderCont;
