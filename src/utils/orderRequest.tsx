b150d2146d1dbbb0fdeba796e6bdd8d4bc22ea7d
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-03-21 14:07:44 +0800

    ''

--- src/utils/orderRequest.tsx
+++ src/utils/orderRequest.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // const baseUrl = "https://food-service-cyan.vercel.app"
 // const baseUrl = "http://localhost:3000"
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+const baseUrl = "https://food-backend-8ud7.onrender.com"
 
 export const getOdrersReq = async (page:number, date:{from:Date, to:Date}) => {
     try {
