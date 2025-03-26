b84a4b46fbf6ccd696c1480cba909db52eb3014c
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-03-26 17:22:05 +0800

    fim

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,27 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import RegistrationForm from "./_features/RegistrationForm";
-import { getUserEmail } from "@/utils/authRequest";
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    const getEmail = async () => {
-      try {
-        const response = await getUserEmail()
-        if (response?.status === 200) {
-          router.push("/");
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getEmail();
-  }, [router]);
-
   return (
     <div className="w-screen flex h-screen items-center">
       <RegistrationForm />
